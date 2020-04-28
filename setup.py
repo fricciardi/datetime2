d04e72518ba9528290f8b439d38e026d14912ff2
fricciardi <francesco.ricciardi@dxc.com>
2020-04-28 18:38:10 +0200

    Added new Python version support
    (and 3.5 was missing)

--- setup.py
+++ setup.py
@@ -27,8 +27,10 @@ setuptools.setup(
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Scientific/Engineering',
           'Topic :: Software Development :: Libraries :: Python Modules'
           ],
