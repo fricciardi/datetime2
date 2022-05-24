52a4ab105d027e1e861550b276f2c4390708769f
fricciardi <francesco.ricciardi@dxc.com>
2022-05-24 17:59:19 +0200

    Bump to 0.8.0dev5
    Because I didn't do it in both places

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Francesco Ricciardi'
 # The short X.Y version
 version = '0.8'
 # The full version, including alpha/beta/rc tags
-release = '0.8.0dev4'
+release = '0.8.0dev5'
 
 
 # -- General configuration ---------------------------------------------------
