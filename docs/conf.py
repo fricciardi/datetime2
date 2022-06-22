9ed45ceac86c07cbdec889d5e2bef232b51730fe
fricciardi <francesco.ricciardi@dxc.com>
2022-06-22 17:05:19 +0200

    Bump version to 0.9.0dev1 before stable release

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Francesco Ricciardi'
 # The short X.Y version
 version = '0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0dev0'
+release = '0.9.0dev1'
 
 
 # -- General configuration ---------------------------------------------------
