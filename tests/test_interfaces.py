efddc0c431b812eeca8b58cff563d063b7751a24
fricciardi <francesco.ricciardi@dxc.com>
2022-07-12 17:09:52 +0200

    Added tests of the TimeDelta interface, plus small fixes

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -33,7 +33,7 @@ __author__ = "Francesco Ricciardi <francescor2010 at yahoo.it>"
 from fractions import Fraction
 import pytest
 
-from datetime2 import Date, Time
+from datetime2 import Date, Time, TimeDelta
 
 
 #############################################################################
@@ -61,7 +61,7 @@ def clean_Date(request):
     request.addfinalizer(clear_Date_class)
 
 
-def test_00_register_new_calendar(clean_Date):
+def test_000_register_new_calendar(clean_Date):
     # valid call
     assert not hasattr(Date, "test_1")
     with pytest.raises(AttributeError):
@@ -106,7 +106,7 @@ def test_00_register_new_calendar(clean_Date):
         Date.register_new_calendar("test_2", NoToCalendar)
 
 
-def test_10_registered_cal_attribute_simple_class(clean_Date):
+def test_010_registered_cal_attribute_simple_class(clean_Date):
     Date.register_new_calendar("test_1", ExampleTestCalendar)
 
     # Date attribute type and metaclass are correct
@@ -136,7 +136,7 @@ def test_10_registered_cal_attribute_simple_class(clean_Date):
     assert d1c.test_1.day == 3
 
 
-def test_12_registered_cal_attribute_class_with_other_constructors(clean_Date):
+def test_012_registered_cal_attribute_class_with_other_constructors(clean_Date):
     class ExampleTestCalendar2(ExampleTestCalendar):
         @classmethod
         def with_thousands(cls, thousands, week, day):
@@ -174,7 +174,7 @@ def test_12_registered_cal_attribute_class_with_other_constructors(clean_Date):
     assert d2c.test_2.day == 3
 
 
-def test_14_registered_cal_attribute_class_with_static_methods(clean_Date):
+def test_014_registered_cal_attribute_class_with_static_methods(clean_Date):
     class ExampleTestCalendar3(ExampleTestCalendar):
         @staticmethod
         def is_odd(number):
@@ -219,7 +219,7 @@ def test_14_registered_cal_attribute_class_with_static_methods(clean_Date):
     assert not d3c.test_3.is_odd(4)
 
 
-def test_16_Date_has_attributes_but_instance_not():
+def test_016_Date_has_attributes_but_instance_not():
     # the date class aways has a registered attribute
     assert hasattr(Date, "gregorian")
     assert Date.gregorian
@@ -241,8 +241,8 @@ def test_16_Date_has_attributes_but_instance_not():
     d3.gregorian
 
 
-def test_90_avoid_date_override():
-    # In the past it happened that a date instance, created via a Gregorian calenader,
+def test_090_avoid_date_override():
+    # In the past it happened that a date instance, created via a Gregorian calendar,
     # was able to directly get an attribute which instead belonged to the calendar class.
     # This is not desirable, better must be thought as an error.
     d = Date.gregorian(1, 1, 1)
@@ -257,7 +257,7 @@ def test_90_avoid_date_override():
 #
 class ExampleTestTimeRepresentation:
     # a fictional representation where there are 100 hours in a day and 100 minutes in an hour
-    # (e.g. a minute in this representation is less than a second in the setern time representation)
+    # (e.g. a minute in this representation is less than a second in the western time representation)
     def __init__(self, hour100, minute100):
         self.hour100 = hour100
         self.minute100 = minute100
@@ -445,7 +445,7 @@ def test_214_registered_time_attribute_class_with_static_methods(clean_Time):
 
 
 def test_216_Time_has_attributes_but_instance_not():
-    # the Time class aways has a registered attribute
+    # the Time class always has a registered attribute
     assert hasattr(Time, "western")
     assert Time.western
     # an instance created with another calendar or by Time does not have
@@ -460,7 +460,7 @@ def test_216_Time_has_attributes_but_instance_not():
         t2.__dict__["western"]
     assert hasattr(t2, "western")
     t2.western
-    # a Time instance created via the calendar does have the same attribute
+    # a Time instance created via the time class does have the same attribute
     t3 = Time.western(3, 4, 5)
     assert hasattr(t3, "western")
     t3.western
@@ -495,3 +495,218 @@ def test_230_naivety_is_preserved():
 
     t4 = Time.test_1(11, 12, utcoffset=0)
     assert t4.utcoffset is not None
+
+
+#############################################################################
+# TimeDelta interface
+#
+class ExampleTestTimeInterval:
+    # a fictional time interval given in days and thousandths of a day
+    def __init__(self, days, thousandths):
+        self.days = days
+        self.thousandths = thousandths
+
+    @classmethod
+    def from_fractional_days(cls, fractional_days):
+        days = int(fractional_days)
+        return cls(days, (fractional_days - days) * 1000)
+
+    def to_fractional_days(self):
+        return self.thousandths / 1000 + self.days
+
+
+@pytest.fixture
+def clean_TimeDelta(request):
+    def clear_TimeDelta_class():
+        for name in [name for name in TimeDelta.__dict__.keys() if name.startswith("test_")]:
+            delattr(TimeDelta, name)
+
+    request.addfinalizer(clear_TimeDelta_class)
+
+
+def test_400_register_new_time_interval(clean_TimeDelta):
+    # valid call
+    assert not hasattr(TimeDelta, "test_1")
+    with pytest.raises(AttributeError):
+        TimeDelta.test_1
+    TimeDelta.register_new_time("test_1", ExampleTestTimeInterval)
+    assert hasattr(TimeDelta, "test_1")
+    TimeDelta.test_1
+
+    # existing time repr or attribute
+    with pytest.raises(AttributeError):
+        TimeDelta.register_new_time("western", ExampleTestTimeInterval)
+    with pytest.raises(AttributeError):
+        TimeDelta.register_new_time("day_frac", ExampleTestTimeInterval)
+
+    # invalid attribute name
+    with pytest.raises(ValueError):
+        TimeDelta.register_new_time("", ExampleTestTimeInterval)
+    with pytest.raises(ValueError):
+        TimeDelta.register_new_time("123new", ExampleTestTimeInterval)
+    with pytest.raises(ValueError):
+        TimeDelta.register_new_time(123, ExampleTestTimeInterval)
+
+    # invalid time repr class
+    class WithoutFromFractional:  # without from_fractional_days
+        def __init__(self, days, thousandths):
+            self.days = days
+            self.thousandths = thousandths
+
+        def to_fractional_days(self):
+            return self.thousandths / 1000 + self.days
+
+    with pytest.raises(TypeError):
+        TimeDelta.register_new_time_interval("test_2", WithoutFromFractional)
+
+    class WithoutToFractional:  # without to_time_pair
+        def __init__(self, days, thousandths):
+            self.days = days
+            self.thousandths = thousandths
+
+        @classmethod
+        def from_fractional_days(cls, fractional_days):
+            days = int(fractional_days)
+            return cls(days, (fractional_days - days) * 1000)
+
+    with pytest.raises(TypeError):
+        TimeDelta.register_new_time_interval("test_2", WithoutToFractional)
+
+
+def test_410_registered_time_attribute_simple_class(clean_TimeDelta):
+    TimeDelta.register_new_time_interval("test_1", ExampleTestTimeInterval)
+
+    # Time attribute type and metaclass are correct
+    assert TimeDelta.test_1.__name__ == "ExampleTestTimeIntervalInTimeDelta"
+    assert type(TimeDelta.test_1).__name__ == "ModifiedClass"
+    assert issubclass(TimeDelta.test_1, ExampleTestTimeInterval)
+
+    # constructed TimeDelta type and value are is correct
+    td1 = TimeDelta.test_1(2, 222)
+    assert type(td1) == TimeDelta
+    assert td1.fractional_days == Fraction(1111, 500)
+
+    # new attribute on TimeDelta instance, type and value are correct
+    td2 = TimeDelta("12511/653")
+    assert isinstance(td2.test_1, ExampleTestTimeInterval)
+    assert type(td2.test_1).__name__ == "ExampleTestTimeIntervalInTimeDelta"
+    assert type(type(td2.test_1)).__name__ == "ModifiedClass"
+    assert td2.test_1.days == 19
+    assert td2.test_1.thousandths == Fraction(104000, 653)
+
+    # new attribute on TimeDelta instance build by another calendar, type and value are correct
+    td3 = TimeDelta.western(12, 23, 34, 45)
+    assert isinstance(td3.test_1, ExampleTestTimeInterval)
+    assert type(td3.test_1).__name__ == "ExampleTestTimeIntervalInTimeDelta"
+    assert type(type(td3.test_1)).__name__ == "ModifiedClass"
+    assert td3.test_1.days == 12
+    assert td3.test_1.thousandths == Fraction("141475/144")
+
+
+def test_412_registered_timedelta_attribute_class_with_other_constructors(clean_TimeDelta):
+    class ExampleTestTimeInterval2(ExampleTestTimeInterval):
+        @classmethod
+        def with_microdays(cls, days, thousandths, udays):
+            return cls(days, thousandths + Fraction(udays, 1000))
+
+    TimeDelta.register_new_time_interval("test_2", ExampleTestTimeInterval2)
+
+    # TimeDelta attribute type and metaclass are correct
+    assert TimeDelta.test_2.__name__ == "ExampleTestTimeInterval2InTimeDelta"
+    assert type(TimeDelta.test_2).__name__ == "ModifiedClass"
+    assert issubclass(TimeDelta.test_2, ExampleTestTimeInterval)
+
+    # constructed TimeDelta type and value are is correct
+    td1 = TimeDelta.test_2(1, 234)
+    assert type(td1) == TimeDelta
+    assert td1.fractional_days == Fraction(617, 500)
+    td2 = TimeDelta.test_2.with_microdays(40, 123, 456)
+    assert type(td2) == TimeDelta
+    assert td2.fractional_days == Fraction(158170, 15625)
+
+    # new attribute on TimeDelta instance, type and value are correct
+    td3 = TimeDelta("1963/1250")
+    assert isinstance(td3.test_2, ExampleTestTimeInterval2)
+    assert type(td3.test_2).__name__ == "ExampleTestTimeInterval2InTimeDelta"
+    assert type(type(td3.test_2)).__name__ == "ModifiedClass"
+    assert td3.test_2.days == 1
+    assert td3.test_2.thousandths == Fraction(2852, 5)
+
+    # new attribute on TimeDelta instance build by another calendar, type and value are correct
+    td4 = TimeDelta.western(9, 10, 11, 12)
+    assert isinstance(td4.test_2, ExampleTestTimeInterval2)
+    assert type(td4.test_2).__name__ == "ExampleTestTimeInterval2InTimeDelta"
+    assert type(type(td4.test_2)).__name__ == "ModifiedClass"
+    assert td4.test_2.days == 9
+    assert td4.test_2.thousandths == Fraction(3820, 9)
+
+
+def test_414_registered_timedelta_attribute_class_with_static_methods(clean_TimeDelta):
+    class ExampleTestTimeInterval3(ExampleTestTimeInterval):
+        @staticmethod
+        def is_odd(number):
+            return (number % 2) == 1
+
+    TimeDelta.register_new_time("test_3", ExampleTestTimeInterval3)
+
+    # Time attribute type and metaclass are correct
+    assert TimeDelta.test_3.__name__ == "ExampleTestTimeInterval3InTimeDelta"
+    assert type(TimeDelta.test_3).__name__ == "ModifiedClass"
+    assert issubclass(TimeDelta.test_3, ExampleTestTimeInterval)
+
+    # constructed Time type and value are is correct
+    td1 = TimeDelta.test_3(3, 789)
+    assert type(td1) == Time
+    assert td1.fractional_days == Fraction(3789, 1000)
+
+    # new attribute on Time instance, type and value are correct
+    td2 = TimeDelta("11250/963")
+    assert isinstance(td2.test_3, ExampleTestTimeInterval3)
+    assert type(td2.test_3).__name__ == "ExampleTestTimeInterval3InTimeDelta"
+    assert type(type(td2.test_3)).__name__ == "ModifiedClass"
+    assert td2.test_3.days == 11
+    assert td2.test_3.thousandths == Fraction(73000, 107)
+
+    # new attribute on Time instance build by another calendar, type and value are correct
+    td3 = TimeDelta.western(12, 11, 10, 9)
+    assert isinstance(td3.test_3, ExampleTestTimeInterval3)
+    assert type(td3.test_3).__name__ == "ExampleTestTimeInterval3InTimeDelta"
+    assert type(type(td3.test_3)).__name__ == "ModifiedClass"
+    assert td3.test_3.days == 12
+    assert td3.test_3.thousandths == Fraction(67015, 144)
+
+    # static method can be reached on the class and on all types of instance
+    assert TimeDelta.test_3.is_odd(3)
+    assert not TimeDelta.test_3.is_odd(4)
+    assert td1.test_3.is_odd(3)
+    assert not td1.test_3.is_odd(4)
+    assert td2.test_3.is_odd(3)
+    assert not td2.test_3.is_odd(4)
+    assert td3.test_3.is_odd(3)
+    assert not td3.test_3.is_odd(4)
+
+
+def test_416_TimeDelta_has_attributes_but_instance_not():
+    # the TimeDelta class always has a registered attribute
+    assert hasattr(TimeDelta, "western")
+    assert TimeDelta.western
+    # an instance created with another time interval or directly by TimeDelta
+    # does not have the attribute; it is instead is reachable via the Time
+    # class
+    td1 = TimeDelta(1234, 567)
+    with pytest.raises(KeyError):
+        td1.__dict__["western"]
+    assert hasattr(td1, "western")
+    td1.western
+    # TODO: if and when we'll have another time interval class we'll add it here
+    # td2 = TimeDelta.internet(345)
+    # with pytest.raises(KeyError):
+    #     td2.__dict__["western"]
+    # assert hasattr(td2, "western")
+    # td2.western
+    # a TimeDelta instance created via the time interval class does have the
+    # same attribute
+    td3 = TimeDelta.western(3, 4, 5)
+    assert hasattr(td3, "western")
+    td3.western
+
